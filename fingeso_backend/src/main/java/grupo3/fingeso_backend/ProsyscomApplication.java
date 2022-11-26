c62b7087fd473efae5237d50fe0aab4a7a3db585
hector-gallard0 <hector.gallardo.a@usach.cl>
2022-11-26 03:23:43 -0800

    asd

---
+++ fingeso_backend/src/main/java/grupo3/fingeso_backend/ProsyscomApplication.java
@@ -0,0 +1,13 @@
+package grupo3.fingeso_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsyscomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsyscomApplication.class, args);
+	}
+
+}
---
+++ fingeso_backend/src/test/java/grupo3/fingeso_backend/ProsyscomApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.fingeso_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsyscomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
